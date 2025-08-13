501da1392a056e4a7e6e4ebb554b9d1ef864fa7c
Àlex Xavier Touza Luque <alex.touza.l@gmail.com>
2025-08-13 16:57:30 +0200

    Extract pre-feature behavior to function pre_call_feature

--- include/feature_adapter.h
+++ include/feature_adapter.h
@@ -11,22 +11,42 @@
 
 namespace dpp_structures {
 
-	template<typename T>
-	concept FeatureSubclass = std::is_base_of_v<feature, T>;
+    namespace detail {
+        dpp::task<void> pre_call_feature(dpp::slashcommand_t event, bool ephemeralThink) {
+            co_await event.co_thinking(ephemeralThink);
 
-	template<auto Function, bool EphemeralThink = false, typename... Args>
-		requires std::is_member_function_pointer_v<decltype(Function)> &&
-				 (std::is_invocable_v<decltype(Function),
-									  typename internal::member_function_pointer_info<decltype(Function)>::class_type,
-									  dpp::slashcommand_t, Args...>) &&
-				 FeatureSubclass<typename internal::member_function_pointer_info<decltype(Function)>::class_type>
-	dpp::task<void> call_feature(dpp::slashcommand_t event, Args... args) {
-		co_await event.co_thinking(EphemeralThink);
+            co_return;
+        }
+    }
 
+    template<typename T>
+    concept FeatureSubclass = std::is_base_of_v<feature, T>;
 
-		using class_type = typename internal::member_function_pointer_info<decltype(Function)>::class_type;
-		co_await std::invoke(Function, class_type::get_instance(), event, args...);
-	}
+    /**
+     *
+     * @tparam Function A pointer to member function.
+     * @tparam EphemeralThink Whether the 'thinking' message should be shown only to the user that triggered the command.
+     * @param event
+     * @param args Arguments passed to the member function, apart from the event.
+     * @return
+     */
+    template<auto Function, bool EphemeralThink = false, typename... Args>
+    requires std::is_member_function_pointer_v<decltype(Function)> &&
+             // Member function pointer is callable using feature instance, event and args passed
+             (std::is_invocable_v<decltype(Function),
+                     typename internal::callable_info<decltype(Function)>::class_type,
+                     dpp::slashcommand_t, Args...>) &&
+             // The class pointed by the member function pointer is a subclass of feature.
+             FeatureSubclass<typename internal::callable_info<decltype(Function)>::class_type>
+    dpp::task<void> call_feature(dpp::slashcommand_t event, Args... args) {
+        co_await dpp_structures::detail::pre_call_feature(event, EphemeralThink);
+
+        using class_type = typename internal::callable_info<decltype(Function)>::class_type;
+
+        co_await std::invoke(Function, class_type::get_instance(), event, args...);
+
+        co_return;
+    }
 
 
 }// namespace dpp_structures
